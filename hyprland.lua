77f948dc0c4208c6590df5e4746743fc430cd0e9
ABetancourt03 <angelob2207@gmail.com>
2026-08-18 14:17:30 -0400

    add hyprland.lua

---
+++ hyprland.lua
@@ -0,0 +1,23 @@
+hl.config({
+	general = {
+		gaps_in = 1,
+		gaps_out = 1,
+		border_size = 2,
+		col = {
+			active_border = "#8d8d8d",
+		},
+	},
+	group = {
+		col = {
+			border_active = "#8d8d8d",
+		},
+	},
+	decoration = {
+		rounding = 0,
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 2,
+		},
+	},
+})
